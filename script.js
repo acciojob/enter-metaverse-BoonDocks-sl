f46e5ffb2a0ef1ec40c0903ff3af97bbd4d0a544
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-17 14:10:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+  // Get the necessary elements by their IDs
+        const statusParagraph = document.getElementById("status");
+        const enterButton = document.getElementById("enterBtn");
+
+        // Add an event listener to the button for the click event
+        enterButton.addEventListener("click", function () {
+            // Update the text of the paragraph to "Entered Metaverse"
+            statusParagraph.innerHTML = "<h1>Entered Metaverse</h1>";
+        });
